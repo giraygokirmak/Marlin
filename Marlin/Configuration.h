71ae4570083efae6dc63c83bd81475acbe3f3485
Giray <giray@gokirmak.gen.tr>
2025-10-10 14:44:12 +0300

    pid fix

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -517,9 +517,9 @@
     #define DEFAULT_Ki_LIST {   1.08,   1.08 }
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
-    #define DEFAULT_Kp 53.98
-    #define DEFAULT_Ki 8.67
-    #define DEFAULT_Kd 84.02
+    #define DEFAULT_Kp 29.10
+    #define DEFAULT_Ki 5.38
+    #define DEFAULT_Kd 39.33
   #endif
 #endif // PIDTEMP
 
