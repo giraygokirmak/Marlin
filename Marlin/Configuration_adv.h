8f51e145a3ab10c7374af439379ad630ac079ae5
Macit Giray Gökırmak <giray@gokirmak.gen.tr>
2025-10-28 15:49:31 +0300

    Thermal Runaway fix for 24v hotend on 12v

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -296,10 +296,10 @@
  * THERMAL_PROTECTION_HYSTERESIS and/or THERMAL_PROTECTION_PERIOD
  */
 #if ENABLED(THERMAL_PROTECTION_HOTENDS)
-  #define THERMAL_PROTECTION_PERIOD 40        // (seconds)
-  #define THERMAL_PROTECTION_HYSTERESIS 4     // (°C)
+  #define THERMAL_PROTECTION_PERIOD 60        // (seconds)  // 40
+  #define THERMAL_PROTECTION_HYSTERESIS 8     // (°C)       // 4
 
-  //#define ADAPTIVE_FAN_SLOWING              // Slow part cooling fan if temperature drops
+  #define ADAPTIVE_FAN_SLOWING              // Slow part cooling fan if temperature drops
   #if BOTH(ADAPTIVE_FAN_SLOWING, PIDTEMP)
     //#define NO_FAN_SLOWING_IN_PID_TUNING    // Don't slow fan speed during M303
   #endif
@@ -316,8 +316,8 @@
    * and/or decrease WATCH_TEMP_INCREASE. WATCH_TEMP_INCREASE should not be set
    * below 2.
    */
-  #define WATCH_TEMP_PERIOD  40               // (seconds)
-  #define WATCH_TEMP_INCREASE 2               // (°C)
+  #define WATCH_TEMP_PERIOD  60               // (seconds) // 40
+  #define WATCH_TEMP_INCREASE 5               // (°C)      // 2
 #endif
 
 /**
