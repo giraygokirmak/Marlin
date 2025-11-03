3e2b6c499b88f68b339ee0da7695f4713d70b0a7
Macit Giray Gökırmak <giray@gokirmak.gen.tr>
2025-11-03 13:45:48 +0300

    bed size changes to max limits Ender 3 Max compat

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1231,8 +1231,8 @@
 // @section machine
 
 // The size of the printable area
-#define X_BED_SIZE 265
-#define Y_BED_SIZE 255
+#define X_BED_SIZE 295
+#define Y_BED_SIZE 295
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
@@ -1240,7 +1240,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 230
+#define Z_MAX_POS 270
 
 /**
  * Software Endstops
